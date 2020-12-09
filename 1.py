34cbea170e6c6a39b7d44051e56bc6430b9542e8
rain <237239045@qq.com>
2020-12-09 21:49:57 +0800

    12-9

--- 1.py
+++ 1.py
@@ -1,24 +1,14 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("./img/000012.jpg")
+image = cv2.imread("./img/3.jpg")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)      # 转为灰度图
 blurred = cv2.GaussianBlur(gray, (15, 15), 0)
-edged = cv2.Canny(blurred, 30, 120)         # 用Canny算子提取边缘
-# cv2.imshow('1', edged)
-# cv2.waitKey()
-image2, contours, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)      # 轮廓检测
-# cv2.imshow('1', image)
-# cv2.waitKey()
-cv2.drawContours(image, contours, -1, (0, 255, 0), 2)    # 绘制轮廓
-# cv2.imshow('1', image)
-# cv2.waitKey()
-k = contours[0]  # 合并所有轮廓
-for i in range(len(contours)):
-    if i == 0:
-        continue
-    m = contours[i]
-    k = np.vstack((k, m))
+edged = cv2.Canny(blurred, 30, 60)
+cv2.imwrite('./result/edged.jpg', edged)         # 用Canny算子提取边缘
+kernel = np.ones((21,21),np.uint8)
+closing = cv2.morphologyEx(edged, cv2.MORPH_CLOSE, kernel)
+cv2.imwrite('./result/closing.jpg', closing) 
 
 rect = cv2.minAreaRect(k)   # 检测轮廓最小外接矩形，得到最小外接矩形的（中心(x,y), (宽,高), 旋转角度）
 box = np.int0(cv2.boxPoints(rect))   # 获取最小外接矩形的4个顶点坐标
